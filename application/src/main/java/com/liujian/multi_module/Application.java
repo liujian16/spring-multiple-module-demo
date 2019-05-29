593a5e4111b04c1407fd22e20888e197efa069c2
liujian <liujian@shanchuan.cn>
2019-05-29 18:29:29 +0800

    add upload test

--- application/src/main/java/com/liujian/multi_module/Application.java
+++ application/src/main/java/com/liujian/multi_module/Application.java
@@ -2,10 +2,12 @@ package com.liujian.multi_module;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages= {"com.liujian"})
 public class Application {
 	public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+	
 }
